826fd8144651133c2c61c92b1343a7b6a28622dc
Jose Chavez <jsergio.chavez@gmail.com>
2022-01-01 14:11:46 -0500

    adds tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,10 @@
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix'
 import type { MetaFunction } from 'remix'
+import styles from './tailwind.css'
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }]
+}
 
 export const meta: MetaFunction = () => {
   return { title: 'New Remix App' }
