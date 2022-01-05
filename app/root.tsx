0d793bc42020e3e963285907928c651f0cd2865d
Jose Chavez <jsergio.chavez@gmail.com>
2022-01-04 19:50:59 -0500

    title

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ export function links() {
 }
 
 export const meta: MetaFunction = () => {
-  return { title: 'New Remix App' }
+  return { title: '💣 Buscaminas!' }
 }
 
 export default function App() {
